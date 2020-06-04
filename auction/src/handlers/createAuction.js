cff428b6552ff7bebd4e61d5829aef698d52890d
Jaehyeon Kim <dottami@gmail.com>
2020-06-04 22:03:25 +0000

    input validation up

--- auction/src/handlers/createAuction.js
+++ auction/src/handlers/createAuction.js
@@ -1,7 +1,9 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 // const AWS = require('aws-sdk');
 // const uuid = require('uuid');
@@ -46,4 +48,5 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({ inputSchema: createAuctionSchema }));
--- auction/src/lib/commonMiddleware.js
+++ auction/src/lib/commonMiddleware.js
@@ -3,6 +3,9 @@ import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpErrorHandler from '@middy/http-error-handler';
 
+// this may be used as a replacement of @middy/http-error-handler
+// https://github.com/dbartholomae/middy-middleware-json-error-handler
+
 export default handler => middy(handler)
   .use([
     httpJsonBodyParser(),
