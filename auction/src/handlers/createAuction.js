49e9712db7a3fab672464791c8e164fa781ee57a
Jaehyeon Kim <dottami@gmail.com>
2020-06-04 04:00:58 +0000

    up createAction

--- auction/src/handlers/createAuction.js
+++ auction/src/handlers/createAuction.js
@@ -1,13 +1,27 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   console.log(event.body);
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
-    title,
+    id: uuid(),
+    title: title,
     status: 'OPEN',
-    createdAt: now.toISOString()
+    createdAt: now.toISOString(),
   };
+
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
